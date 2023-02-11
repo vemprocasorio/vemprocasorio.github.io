e390e9ac068e8f8413683192f34c82dc86199094
Gustavo Padovan <gustavo.padovan@collabora.com>
2023-02-11 17:46:46 -0300

    Updates

--- component---src-pages-404-js-a5f56df63a82d299b8b7.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{214:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(213),s=n(212);t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(s.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-a5f56df63a82d299b8b7.js.map
