c40a9f3874c67a2a48b07a80c3c49bac5c11ab57
Gustavo Padovan <gustavo.padovan@collabora.com>
2023-02-11 18:11:59 -0300

    more updates

--- src/components/sections/Festa.js
+++ src/components/sections/Festa.js
@@ -20,12 +20,14 @@ const Festa = () => (
                         <h4>Endereço: Avenida Paulista, 735</h4>
                         <h3>Traje</h3>
                         <p>Venha com uma ótima roupa de festa de casamento bem confortável.
-                            Gravatas não são necessárias!
+                            <br />Gravatas não são necessárias!
                         </p>
 
 
                     </Container>
+                    <Art>
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.039820842093!2d-46.65243688388906!3d-23.56701326768877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59b8958a0afd%3A0xed6dff4b578f94d3!2sClub%20Homs!5e0!3m2!1sen!2sbr!4v1676146749609!5m2!1sen!2sbr" width="300" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </Art>
                 </Grid>
         </Container>
         </HeaderWrapper>
@@ -42,21 +44,12 @@ const HeaderWrapper = styled.header`
 `;
 
 const Art = styled.figure`
-  width: 100%;
-  margin: 0;
-  border-radius: 20px;
-  border: 4px solid #fff;
-  > div {
-    @media (max-width: ${props => props.theme.screen.md}) {
-      width: 100%;
-      border-radius: 10px;
-    }
   }
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 2fr 1fr;
   align-items: center;
   grid-gap: 64px;
   @media (max-width: ${props => props.theme.screen.md}) {
